6ac0cbf2761f2563379def8910ba2eb05e9ad569
grant956 <68757554+Psihodel666@users.noreply.github.com>
2022-09-13 21:20:13 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -7,26 +7,40 @@ import throttle from 'lodash.throttle';
     btnStop : document.querySelector('button[data-stop]'),
     body : document.querySelector('body')
   }
+
+  let isActive = false;
   
 
   refs.btnStart.addEventListener('click', throttle(start,1000));
   refs.btnStop.addEventListener('click', stop);
   refs.btnStop.setAttribute('disabled', true)
 
-
+  function getRandomHexColor() {
+    return  `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+   
+  }
 
   function start(){
+    isActive = false;
    refs.btnStart.setAttribute('disabled', true);
    refs.btnStop.removeAttribute('disabled')
-
-    colorBody = setInterval(() => {
-      refs.body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-    }, 1000);
+   
+      colorBodyInterval = setInterval(() =>{
+        if(isActive === true){
+          clearInterval(colorBody)  
+          return;
+        }
+        
+        const randomColor =  getRandomHexColor();
+        document.body.style.background = randomColor;
+      },  1000);
   }
 
   
   function stop(){
     refs.btnStart.removeAttribute('disabled');
-    clearInterval(colorBody);
+    clearInterval(colorBodyInterval);
     refs.btnStop.setAttribute('disabled', true)
+    isActive = false;
+    console.log(isActive)
   }
